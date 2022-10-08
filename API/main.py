65cbcf90306a86e0c46673b832fa183e32f8715c
Aspy <52931655+AspySs@users.noreply.github.com>
2022-10-08 04:35:09 +0300

    structure
    +add parser bot
    +add two endpoints to API

---
+++ API/main.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import request
+import sqlite3
+import json
+
+conn = sqlite3.connect('../data.sqlite', check_same_thread=False)
+cur = conn.cursor()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/get_tags/', methods=['GET'])
+def get_tgs():
+    res = cur.execute("""SELECT DISTINCT * FROM tags""")
+    result = res.fetchall()
+    return json.dumps(result)
+
+@app.route('/get_text/', methods=['GET'])
+def get_txts():
+    tag = request.args.get('tag')
+    res = cur.execute("SELECT post_id FROM tags WHERE tag = ?", (tag, ))
+    result_id = res.fetchall()
+    lst = []
+    for i in result_id:
+        res = cur.execute("SELECT text FROM posts WHERE id = ?", (i[0], )).fetchone()
+        lst.append(res)
+    return json.dumps(lst)
+
+
+if __name__ == '__main__':
+    app.run()
--- BOT/main.py
+++ BOT/main.py
@@ -1,10 +1,33 @@
-import telebot
 import time
-# Токен, который выдает @botfather
-bot = telebot.TeleBot('5690661059:AAEtuZiAni00e3dKN5yKR45RCbrVdAPOcVc')
-# Адрес телеграм-канала, начинается с @
-CHANNEL_NAME = '@buznews'
-# Загружаем список шуток
-bot.send_message(CHANNEL_NAME, "test")
-# Делаем паузу в один час
-time.sleep(3600)
+from telegram.ext import Updater, MessageHandler, Filters
+import sqlite3
+
+conn = sqlite3.connect('../data.sqlite', check_same_thread=False)
+cur = conn.cursor()
+
+
+KEY = "5690661059:AAEtuZiAni00e3dKN5yKR45RCbrVdAPOcVc"
+updater = Updater(token=KEY, use_context=True)
+dispatcher = updater.dispatcher
+
+
+def forwarder(update, context):
+    msg = update.channel_post
+    tag_source = msg.text
+    tags = {tag.strip("#") for tag in tag_source.split() if tag.startswith("#")}
+
+    c = cur.execute("""INSERT INTO posts VALUES (NULL, ?, ?)""", (msg.text, time.time()))
+    id_post = c.lastrowid
+    conn.commit()
+
+    for t in tags:
+        cur.execute("""INSERT INTO tags VALUES (NULL, ?, ?)""", (id_post, t))
+        conn.commit()
+
+
+
+forwardHandler = MessageHandler(Filters.text & (~Filters.command), forwarder)
+dispatcher.add_handler(forwardHandler)
+updater.start_polling()
+updater.idle()
+conn.close()
